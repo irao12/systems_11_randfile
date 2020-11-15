73332bd8219d9c5be3accf75db6c767115112ac0
ivan <ivanrao@outlook.com>
2020-11-15 12:08:45 -0500

    added

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int random_int(int input){
+	int result;
+	read(input, &result, sizeof(int));
+	return result;
+}
+
+int main(){
+	int  input = open("/dev/random", O_RDONLY);
+	int write_to = open("data", O_CREAT | O_RDWR);
+	int rand_ints[10];
+	int i;
+	printf("Generating random numbers:\n");
+	for (i = 0;  i < 10; i++){
+		rand_ints[i] = random_int(input);
+	}
+	for (i = 0; i < 10; i++){
+		printf("\trandom %d: %d\n", i, rand_ints[i]);
+	}
+
+	printf("\nWriting numbers to file...\n");
+	int bytes = write(write_to, &rand_ints, sizeof(rand_ints));
+	
+	printf("\nReading numbers from file...\n");
+	int rand_ints_2[10];
+
+	return 0;
+}
